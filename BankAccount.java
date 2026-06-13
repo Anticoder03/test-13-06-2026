94d6cc58bc76a365291f3f8cf4b84ff80e272ec6
Anticoder03 <ap5381545@gmail.com>
2026-06-13 20:51:11 +0530

    first

---
+++ BankAccount.java
@@ -0,0 +1,28 @@
+package test06;
+
+public class BankAccount {
+	private  String ACC_NO,Holdername;
+	private double Bal;
+	
+	
+	public String getACC_NO() {
+		return ACC_NO;
+	}
+	public void setACC_NO(String aCC_NO) {
+		ACC_NO = aCC_NO;
+	}
+	public String getHoldername() {
+		return Holdername;
+	}
+	public void setHoldername(String holdername) {
+		Holdername = holdername;
+	}
+	public double getBal() {
+		return Bal;
+	}
+	public void setBal(double bal) {
+		Bal = bal;
+	}
+	
+	
+}
